19f2b01db6a976b601ac1d36537d129f1beaee43
minakshi1506 <15minakshitiwari@gmail.com>
2023-12-23 20:17:06 +0530

    new commit

--- App.js
+++ App.js
@@ -1,1 +1,3 @@
+prompt('you have shirt?') ==='blue shirt' ? prompt ('you have' ?) ==='black jean'?
+console.log('do you have black formal pants'?); console.log('you.re invited to the Sam's party') ; console.log('not a mach') ;
 
